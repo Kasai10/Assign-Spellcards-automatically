be74c97145c822c401ba7b7a7e1dc2b884adc780
Simo Eckert <simo.eckert@stud.fra-uas.de>
2024-10-28 17:06:16 +0100

    kekse

--- Assign-spellcards.py
+++ Assign-spellcards.py
@@ -5,91 +5,79 @@ import shutil
 import glob
 import sys
 
+#kekse
 arguments = sys.argv
-
-
 
 def copy_to_target_location(path, is_Spellcards):
     # Destination path
     destination = dnd_folder_path
     if is_Spellcards:
         shutil.copy(path, destination)
-    elif not is_Spellcards:
+    else:
         shutil.copy(path, character_sheets_dir)
 
-if("-edit" in arguments):
+if "-edit" in arguments:
     dnd_folder_path = input("Input the path to your dnd folder: ")
     with open("Dndpath.txt", "w") as file:
         file.write(dnd_folder_path)
 try:
     with open("Dndpath.txt", "r") as file:
-        dnd_folder_path = file.readline()
-except:
+        dnd_folder_path = file.readline().strip()
+except FileNotFoundError:
     print("Use -edit the first time you start in order to provide a dnd folder path.")
     quit()
+
 character_sheets_dir = os.path.join(dnd_folder_path, "CharacterSheets")
 character_name = ""
 try:
     characters = os.listdir(character_sheets_dir)
-    for i in range(len(characters)):
-        characters[i] = characters[i].replace(".pdf", "")
-
-
-        while(True):
-            print("Press 'a' if you want to add a character to the directory!") 
-            character_name = input(f"Input character name, the following files are present in the directory: {characters}: ")
-            if character_name == "a":
-                path_to_charactersheet = input("Input the path to the charactersheet here: ")
-                try:
-                    copy_to_target_location(path_to_charactersheet, False)
-                    continue
-                except:
-                    print("Sheet not found. Please try again!")
-                    continue
-            spells_for_character = os.path.join(dnd_folder_path, "Spellcards " + character_name)
-            if character_name in characters:
-                break
-            else:
-                print("Please input a valid character")
-except:
+    characters = [filename.replace(".pdf", "") for filename in characters]
+    while True:
+        print("Press 'a' if you want to add a character to the directory!") 
+        character_name = input(f"Input character name, the following files are present in the directory: {characters}: ")
+        if character_name == "a":
+            path_to_charactersheet = input("Input the path to the charactersheet here: ")
+            try:
+                copy_to_target_location(path_to_charactersheet, False)
+                continue
+            except FileNotFoundError:
+                print("Sheet not found. Please try again!")
+                continue
+        spells_for_character = os.path.join(dnd_folder_path, f"Spellcards {character_name}")
+        if character_name in characters:
+            break
+        else:
+            print("Please input a valid character")
+except FileNotFoundError:
     print("Making directories now...")
 
-    
-
-spellcards_folder = os.path.join(dnd_folder_path, "Spell Cards")
+# Use the directory where the script is located for spellcards_folder
+spellcards_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Spell Cards")
 
 def make_spellcard_folder(character_name):
     # Create dnd_folder_path if it doesn't exist
     if not os.path.exists(dnd_folder_path):
         os.makedirs(dnd_folder_path)
-        print(dnd_folder_path + " successfully created")
+        print(f"{dnd_folder_path} successfully created")
     
     # Create character_sheets_dir if it doesn't exist
     if not os.path.exists(character_sheets_dir):
         os.makedirs(character_sheets_dir)
-        print(character_sheets_dir + " successfully created")
+        print(f"{character_sheets_dir} successfully created")
     
     # Create spells_for_character if it doesn't exist
-    try:
-        if not os.path.exists(spells_for_character):
-            os.makedirs(spells_for_character)
-            print(spells_for_character + " successfully created")
-        else:
-            print("Character already exists")
-    except:
-        spellcards_path = input("Path to Spellcards folder: ")
-        copy_to_target_location(spellcards_path, True)
-        character_sheet_location = input("Location of character sheet: ")
-        copy_to_target_location(character_sheet_location, False)
-        print(f"Directories were successfully created")
-        make_spellcard_folder()
+    if not os.path.exists(spells_for_character):
+        os.makedirs(spells_for_character)
+        print(f"{spells_for_character} successfully created")
+    else:
+        print("Character already exists")
     
     get_character_sheet_text(character_name)
 
 def get_character_sheet_text(character_name):
-    character_sheet_path = os.path.join(character_sheets_dir, character_name + ".pdf")
+    character_sheet_path = os.path.join(character_sheets_dir, f"{character_name}.pdf")
     if not os.path.exists(character_sheet_path):
-        print("The character sheet you provided does not exist within " + character_sheets_dir)
+        print(f"The character sheet you provided does not exist within {character_sheets_dir}")
         return
 
     character_sheet = fitz.open(character_sheet_path)
@@ -106,42 +94,24 @@ def get_character_sheet_text(character_name):
     cleaned_spells = []
     for spell in spells:
         if spell.startswith("O"):
-            spell.strip()
+            spell = spell.strip()
             # Remove the leading 'O' and strip any whitespace
             cleaned_spell = spell[1:].strip()
             cleaned_spells.append(cleaned_spell)
 
     # Print the cleaned spells
-    double_cleaned_spells = []
-    for spell in cleaned_spells:
-        if spell.startswith("O"):
-            spell.strip()
-            # Remove the leading 'O' and strip any whitespace
-            cleaned_spell = spell[1:].strip()
-            double_cleaned_spells.append(cleaned_spell)
-        else:
-            double_cleaned_spells.append(spell)
-    
-    copy_spells_in_spellfolder(double_cleaned_spells)
-
-    
-def clean_file_name(file_name):
-    # Remove any leading or trailing whitespace
-    file_name = file_name.strip()
-    # Remove any additional characters that may be causing the discrepancy
-    file_name = file_name.replace("  ", " ")  # Replace double spaces with single space
-    # Optionally, you can remove any other unwanted characters or patterns
-    
-    return file_name
+    copy_spells_in_spellfolder(cleaned_spells)
 
 def copy_spells_in_spellfolder(potential_spells):
-    spellcards_folder = os.path.join(dnd_folder_path, "Spell Cards")
     spells_for_character = os.path.join(dnd_folder_path, f"Spellcards {character_name}")
 
     # Create the spells_for_character folder if it doesn't exist
     if not os.path.exists(spells_for_character):
         os.makedirs(spells_for_character)
         print(f"{spells_for_character} successfully created")
+
+    # Store the names of copied files to avoid duplication
+    copied_files = set()
 
     # Iterate through potential_spells and search for corresponding PNG files
     for spell_name in potential_spells:
@@ -158,12 +128,15 @@ def copy_spells_in_spellfolder(potential_spells):
             png_files = glob.glob(png_file_pattern)
 
         if png_files:
-            # If PNG files are found, copy the first one (assuming unique file names)
-            source_path = png_files[0]
-            file_name = os.path.basename(source_path)
-            destination_path = os.path.join(spells_for_character, file_name)
-            shutil.copy(source_path, destination_path)
-            print(f"Copied {file_name} to {spells_for_character}")
+            # Copy the first PNG file found
+            for png_file in png_files:
+                file_name = os.path.basename(png_file)
+                # Ensure the file hasn't been copied already
+                if file_name not in copied_files:
+                    destination_path = os.path.join(spells_for_character, file_name)
+                    shutil.copy(png_file, destination_path)
+                    copied_files.add(file_name)
+                    print(f"Copied {file_name} to {spells_for_character}")
         else:
             print(f"No PNG file found for {spell_name}")
 
